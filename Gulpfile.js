2db93529ffc8940242da7756b59ee125131d493e
asxelot <asxelot@gmail.com>
2015-10-30 15:40:06 +0200

    gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,35 +1,17 @@
 var gulp       = require('gulp'),
-    cssmin     = require('gulp-cssmin'),
     ngAnnotate = require('gulp-ng-annotate'),
     sourcemaps = require('gulp-sourcemaps'),
     uglify     = require('gulp-uglify'),
     concat     = require('gulp-concat'),
-    rename     = require('gulp-rename'),
-    gutil      = require('gulp-util'),
-    prefix     = require('gulp-autoprefixer'),
     livereload = require('gulp-livereload')
 
 var src = {
-  css: ['css/*.css', '!css/*.min.css'],
   html: ['views/*.html', 'index.html'],
   js  : 'app/**/*.js'
 }
 
-function onError(err) {
-  console.error(err)
-  gutil.beep()
-}
-
 gulp.task('html', function() {
   gulp.src('index.html')
-      .pipe(livereload())
-})
-
-gulp.task('css', function() {
-  gulp.src(src.css)  
-      .pipe(prefix())
-      .pipe(cssmin())
-      .pipe(gulp.dest('css'))
       .pipe(livereload())
 })
 
